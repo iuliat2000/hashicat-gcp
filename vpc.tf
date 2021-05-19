77e10f00a495c34876c58ff337b401a8efd08396
Iulia T <iulia.tomescu@gmail.com>
2021-05-19 16:58:48 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -7,12 +7,12 @@ module "vpc" {
     routing_mode = "GLOBAL"
 
     subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+        {
+            subnet_name   = "gaurav-subnet"
+            subnet_ip     = "10.100.10.0/24"
+            subnet_region = var.region
+        }
+
     ]
 
     secondary_ranges = {
