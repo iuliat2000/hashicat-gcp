5770d1acf2bae8e4c3ff9fa2289a8b7d06e98b39
Iulia T <iulia.tomescu@gmail.com>
2021-05-19 17:32:04 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/iuliatudor-training/network/google"
-  version = "2.5.0"
+  version = "3.2.2"
   # insert required variables here
   network_name = "gaurav-network"
   project_id   = var.project
